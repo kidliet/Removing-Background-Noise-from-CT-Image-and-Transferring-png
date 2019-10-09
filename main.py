2f781451139cada66fe866099c284dbfb42438bd
kidliet <36845892+kidliet@users.noreply.github.com>
2019-10-09 09:32:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ ImageType2dF=itk.Image[itk.F,2]
 
 
 def body_slice(image):
-
+    #  get target mask for removing background noise
+    
     image=itk.GetImageFromArray(image.astype(np.float32))
 
     ctBackgroundRange=5
@@ -55,45 +56,6 @@ def body_slice(image):
     thresholdFilter.SetInsideValue(0)
     maskBody=itk.GetArrayFromImage(thresholdFilter.GetOutput())
     return maskBody
-
-
-def load_patient(src_dir):
-    '''
-        读取某文件夹内的所有dicom文件
-    :param src_dir: dicom文件夹路径
-    :return: dicom list
-    '''
-    files = os.listdir(src_dir)
-    print ('files:',files)
-    slices = []
-    for s in files:
-        if is_dicom_file(src_dir + '/' + s):
-            instance = dicom.read_file(src_dir + '/' + s)
-            slices.append(instance)
-    slices.sort(key=lambda x: int(x.InstanceNumber))
-    try:
-        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
-    except:
-        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
-
-    for s in slices:
-        s.SliceThickness = slice_thickness
-    return slices
-def get_pixels_hu_by_simpleitk(dicom_dir):
-    '''
-        读取某文件夹内的所有dicom文件,并提取像素值(-4000 ~ 4000)
-    :param src_dir: dicom文件夹路径
-    :return: image array
-    '''
-    reader = SimpleITK.ImageSeriesReader()
-    dicom_names = reader.GetGDCMSeriesFileNames(dicom_dir)
-    reader.SetFileNames(dicom_names)
-    image = reader.Execute()
-    img_array = SimpleITK.GetArrayFromImage(image)
-    #max ,min = np.max(img_array),np.min(img_array)
-    #print ('img_array',np.max(img_array),np.min(img_array))
-    #img_array[img_array == -2000] = 0
-    return img_array
 def normalize_hu(image):
     '''
            将输入图像的像素值(-1000 ~ 400)归一化到0~1之间
@@ -124,15 +86,13 @@ if __name__ == '__main__':
     dicom_dir = 'E:/data-101/CT_dim/'
     dataset = ospath(dicom_dir)
     count = 0
-    #print (len(dataset),dataset)
+    
     for i in tqdm(range(len(dataset))):
         img_path = "E:/data-101/CT_png/" + str(count).rjust(5, '0') + ".png"
         ds = itk_image_series_reader(dataset[i])  # read dicom
-        ds = itk.GetArrayFromImage(ds)  #get array
+        ds = itk.GetArrayFromImage(ds)  #get array  
         org_img = normalize_hu(ds)    # normalization
-        #print (org_img)
         maskbody = body_slice(ds)     # get mask (0,1)
-        final_img = (org_img*255)*maskbody
-        #final_img = final_img.astype(np.int16)
-        cv2.imwrite(img_path, final_img)
+        final_img = (org_img*255)*maskbody   #Target Segmentation
+        cv2.imwrite(img_path, final_img)    # write png
         count +=1
